c1130710ac181c93150b9172bbdab922b12da6a8
Bereket Ketema <bereketketema866@gmail.com>
2026-02-05 15:49:18 +0300

    Add duplicate finding method in Solution class
    Implement a method to find duplicates in an array.

---
+++ all duplicate in array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        store = []
+        l, r = 0, 1
+        while r < len(nums):
+            if nums[l] == nums[r]:
+                store.append(nums[l])
+            l += 1; r += 1
+        return store
